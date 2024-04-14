c042494f9fb2d06a7764c9584132e9f20666c8f3
amey1302 <ameykulkarni1322@gmail.com>
2024-04-14 17:16:26 +0530

    Update InterGalacticTransactionService to handle the test cases

--- src/main/java/org/amaap/merchantsguidetogalaxy/service/InterGalacticTransactionService.java
+++ src/main/java/org/amaap/merchantsguidetogalaxy/service/InterGalacticTransactionService.java
@@ -1,7 +1,19 @@
 package org.amaap.merchantsguidetogalaxy.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class InterGalacticTransactionService {
     public double calculateCredits(String query) {
         return 68;
+    }
+    public String readFile(String filePath) throws IOException {
+        try {
+            return Files.readString(Paths.get(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
